eab8c8ff6b4c48c4bb8dee4c032262199e021eb0
Powell Quiring <powellquiring@Powells-MacBook-Air.local>
2025-06-27 11:08:13 -0700

    debug github actions 3 change dist to out

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'dist',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
